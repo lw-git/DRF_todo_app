5fe89a2674bfbebddd4ee9e7d9f90b5a16071c47
lw <lw_git@mail.ru>
2020-05-12 10:13:59 +0500

    Added views and urls for API

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.TaskListAPIView.as_view(), name='task-list')
+    path('', views.TaskListAPIView.as_view(), name='task-list'),
+    path('create/', views.TaskCreateAPIView.as_view(), name='task-create'),
+    path('<int:id>/', views.TaskDetailAPIView.as_view(), name='task-detail'),
+    path('<int:id>/update/', views.TaskUpdateAPIView.as_view(), name='task-update'),
+    path('<int:id>/delete/', views.TaskDeleteAPIView.as_view(), name='task-delete'),
 ]
--- todo/api/views.py
+++ todo/api/views.py
@@ -6,3 +6,26 @@ from .serializers import TaskSerializer
 class TaskListAPIView(generics.ListAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+
+class TaskDetailAPIView(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
+
+
+class TaskCreateAPIView(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskUpdateAPIView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
+
+
+class TaskDeleteAPIView(generics.DestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
