c0b548dc3ae7868f8999a06b1ffb213b9df0177a
lw <lw_git@mail.ru>
2020-05-17 10:57:01 +0500

    Added tests for create, update, delete api view

--- todo/api/tests.py
+++ todo/api/tests.py
@@ -23,3 +23,30 @@ class TaskAPITestCase(APITestCase):
         self.assertEqual(task['id'], 1)
         self.assertEqual(task['title'], 'test1')
         self.assertFalse(task['completed'])
+
+    def test_task_create_api_view(self):
+        data = {
+            'title': 'New todo'
+        }
+        response = self.client.post(api_reverse('task-create'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 3)
+
+    def test_task_update_api_view(self):
+        data = {
+            'title': 'New todo',
+            'completed': True
+        }
+        response = self.client.patch(api_reverse('task-update', kwargs={'id': 1}), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Task.objects.count(), 2)
+        updated_task = Task.objects.get(id=1)
+        self.assertEqual(updated_task.title, 'New todo')
+        self.assertTrue(updated_task.completed)
+
+    def test_task_delete_api_view(self):
+        response = self.client.delete(api_reverse('task-delete', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get(api_reverse('task-detail', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(Task.objects.count(), 1)
